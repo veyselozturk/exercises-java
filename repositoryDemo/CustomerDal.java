0fe8739a5269dfd660e18acf0bbb89397f5f1e68
Veysel Öztürk <veyselozturk.6198@gmail.com>
2022-12-21 14:02:38 +0300

    Create CustomerDal.java

---
+++ repositoryDemo/CustomerDal.java
@@ -0,0 +1,17 @@
+package repositoryDemo;
+
+public class CustomerDal implements IEntityRepository<Customer>{
+	// DAL : data access layer veri erişim katmanı
+	
+	@Override
+	public void add(Customer entity) {
+	}
+
+	@Override
+	public void delete(Customer entity) {
+	}
+
+	@Override
+	public void update(Customer entity) {
+	} 
+}
